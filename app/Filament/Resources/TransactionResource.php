79efc48e8d549e2fbe948e00e599f0e1266b1fa0
diraoktap <diraoktap.me@gmail.com>
2025-03-04 00:26:18 +0700

    Daftar Penumpang

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -6,7 +6,10 @@ use App\Filament\Resources\TransactionResource\Pages;
 use App\Filament\Resources\TransactionResource\RelationManagers;
 use App\Models\Transaction;
 use Filament\Forms;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
@@ -25,7 +28,33 @@ class TransactionResource extends Resource
     {
         return $form
             ->schema([
-                Section::make('Rate Limiting')
+                Section::make('Informasi Umum')
+                ->schema([
+                    TextInput::make('code'),
+
+                    Select::make('flight_id')
+                    ->relationship('flight', 'flight_number'),
+                    Select::make('flight_class_id')
+                    ->relationship('class', 'class_type'),
+                ]),
+
+                Section::make('Informasi Penumpang')
+                ->schema([
+                    TextInput::make('name'),
+                    TextInput::make('email'),
+                    TextInput::make('phone'),
+                    Section::make('Daftar Penumpang')
+                    ->schema([
+                        Repeater::make('passenger')
+                        ->relationship('passengers')
+                        ->schema([
+                            TextInput::make('seat.name'),
+                            TextInput::make('name'),
+                            TextInput::make('date_of_birth'),
+                            TextInput::make('nationality'),
+                        ])
+                    ]),
+                ]),
             ]);
     }
 
